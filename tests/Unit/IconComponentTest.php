625b87b688ebe7457edb37de54cd4cb0b42ef7a6
ph7jack <pedrolivertwd@gmail.com>
2025-03-02 18:58:40 -0300

    make tests faster

--- tests/Unit/IconComponentTest.php
+++ tests/Unit/IconComponentTest.php
@@ -1,6 +1,5 @@
 <?php
 
-use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\{Collection, Str};
 use Symfony\Component\Finder\{Finder, SplFileInfo};
 use WireUi\PhosphorIcons\Icon;
@@ -40,41 +39,41 @@ it('should get the correct icon variant', function (string $expected, Icon $icon
     expect($parsedStyle)->toBe($expected);
 })->with([
     ['regular', new Icon(name: 'house', variant: 'regular')],
-    ['thin',    new Icon(name: 'house', thin: true)],
-    ['light',   new Icon(name: 'house', light: true)],
+    ['thin', new Icon(name: 'house', thin: true)],
+    ['light', new Icon(name: 'house', light: true)],
     ['regular', new Icon(name: 'house', regular: true)],
-    ['fill',    new Icon(name: 'house', fill: true)],
+    ['fill', new Icon(name: 'house', fill: true)],
     ['duotone', new Icon(name: 'house', duotone: true)],
-    ['bold',    new Icon(name: 'house', bold: true)],
+    ['bold', new Icon(name: 'house', bold: true)],
 ]);
 
-it('should render all components with attributes', function (string $icon, string $variant) {
-    $html = Blade::render(<<<BLADE
-        <x-icon name="{$icon}" variant="{$variant}" class="w-5 h-5" style="foo: bar" />
+it("should render all variant icons", function (string $variant) {
+    foreach (getIcons($variant) as $data) {
+        $icon = $data['icon'];
 
-        <x-phosphor.icons::{$variant}.{$icon} class="w-10 h-10" />
-    BLADE);
+        $html = Blade::render(<<<BLADE
+            <x-icon name="{$icon}" variant="{$variant}" class="w-5 h-5" style="foo: bar" />
 
-    $view = (new Icon(name: $icon, variant: $variant))->render();
+            <x-phosphor.icons::{$variant}.{$icon} class="w-10 h-10" />
+        BLADE);
 
-    expect($view->name())->toBe("phosphor.icons::components.{$variant}.{$icon}");
+        $view = (new Icon(name: $icon, variant: $variant))->render();
 
-    expect($html)
-        ->toContain('<svg')
-        ->toContain('</svg>')
-        ->toContain('class="w-5 h-5"')
-        ->toContain('class="w-10 h-10"')
-        ->toContain('style="foo: bar"')
-        ->not->toContain('<x-icon')
-        ->not->toContain('<x-phosphor.icons');
-})->with(
-    collect()
-        ->push(getIcons('thin'))
-        ->push(getIcons('light'))
-        ->push(getIcons('regular'))
-        ->push(getIcons('fill'))
-        ->push(getIcons('duotone'))
-        ->push(getIcons('bold'))
-        ->collapse()
-        ->toArray(),
-);
+        expect($view->name())->toBe("phosphor.icons::components.{$variant}.{$icon}")
+            ->and($html)
+            ->toContain('<svg')
+            ->toContain('</svg>')
+            ->toContain('class="w-5 h-5"')
+            ->toContain('class="w-10 h-10"')
+            ->toContain('style="foo: bar"')
+            ->not->toContain('<x-icon')
+            ->not->toContain('<x-phosphor.icons');
+    }
+})->with([
+    'thin',
+    'light',
+    'regular',
+    'fill',
+    'duotone',
+    'bold',
+]);
